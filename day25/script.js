3fcb8b8f008f87e5a1c04d1f213ad8dcfca0794d
Nirajan Kunwor <nirajankunwor604@gmail.com>
2023-09-25 18:02:27 +0545

    feat: login signup, api call

--- day25/script.js
+++ day25/script.js
@@ -12,34 +12,67 @@ const password = document.getElementById('password');
 
 const loginBtn = document.getElementById('login-btn');
 
-loginBtn.addEventListener('click', (event) => {
-   event.preventDefault()
-   let data = {
-      email: email.value,
-      password: password.value
-   }
 
-   // Change object to json using json.stringify method
-
-
-   const loginForm = async () => {
-      const resp = await fetch('https://e8a1-27-34-30-0.ngrok.io/users/login', {
-         method: "POST",
-         headers: { 'Content-Type': 'application/json' },
-         body: JSON.stringify(data)
-      });
-      const loginResp = await resp.json();
-
-      if (loginResp.status) {
-         document.getElementById('error-message').innerText = loginResp.message;
-      } else {
-         console.log(loginResp)
-         document.getElementById('error-message').innerText = loginResp.message;
+if (loginBtn && email && password) {
+   loginBtn.addEventListener('click', (event) => {
+      event.preventDefault()
+      let data = {
+         email: email.value,
+         password: password.value
       }
 
-      console.log(loginResp);
+      // Change object to json using json.stringify method
+      const loginForm = async () => {
+         const resp = await fetch('https://backend-mu-pied.vercel.app/users/login', {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+               "Content-Type": "application/json"
+            }
+         });
+         const loginResp = await resp.json();
+
+
+         if (loginResp.status) {
+            window.location.href = 'dashboard.html';
+         } else {
+            document.getElementById('error-message').innerText = loginResp.message;
+         }
+
+      }
+
+      loginForm();
+
+   })
+}
+
+
+const signupHandler = async (event) => {
+   event.preventDefault();
+
+   const fullName = document.querySelector('#fullName').value;
+   const email = document.querySelector('#signup-email').value;
+   const password = document.querySelector('#signup-password').value;
+
+   const data = {
+      fullName: fullName,
+      email: email,
+      password: password
    }
 
-   loginForm();
+   const resp = await fetch('https://backend-mu-pied.vercel.app/users/register', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+         "Content-Type": "application/json"
+      }
+   })
 
-})
+   const signupResp = await resp.json();
+
+   if (signupResp.status) {
+      window.location.href = "login.html";
+   } else {
+      document.getElementById('error-msg').innerText = signupResp.message;
+   }
+}

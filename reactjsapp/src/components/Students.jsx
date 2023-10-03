2138691483ff74b8486dd1acf7b515c4c0bb5be0
Nirajan Kunwor <nirajankunwor604@gmail.com>
2023-10-03 17:57:52 +0545

    feat: dynamic table generation

--- reactjsapp/src/components/Students.jsx
+++ reactjsapp/src/components/Students.jsx
@@ -2,7 +2,40 @@ import { Table } from "react-bootstrap";
 
 const Students = ({ students }) => {
   //Header for data
+
+  // total students = 30 -> object - {firstName:abc,lastName:'}
   const tableHeaders = Object.keys(students[0]);
+
+  function abc() {
+    const nums = [1, 2, 3, 4, 5];
+    nums.forEach((num) => {
+      console.log(num);
+    });
+  }
+
+  // data["color"]
+  // data["color"]
+
+  const displayTableObjectData = (data) => {
+    return (
+      <table className="table table-bordered">
+        <tbody>
+          {Object.keys(data).map((key, index) => {
+            return (
+              <tr key={index}>
+                <td>{key}</td>
+                <td>
+                  {typeof data[key] === "object"
+                    ? displayTableObjectData(data[key])
+                    : data[key]}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  };
   return (
     <Table striped bordered hover>
       <thead>
@@ -13,12 +46,21 @@ const Students = ({ students }) => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
+        {students.map((student, index) => {
+          return (
+            <tr key={student.id}>
+              {Object.values(students[index]).map((value) => {
+                return (
+                  <td key={value}>
+                    {typeof value === "object"
+                      ? displayTableObjectData(value)
+                      : value}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
       </tbody>
     </Table>
   );
